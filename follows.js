3938d69a8fc416f79242c3ae3d1c376726ea75b0
SJ-optimizer <SJ-optimizer@users.noreply.github.com>
2015-07-10 12:11:11 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [61916], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [61916,12425], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'SJOs Application', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
